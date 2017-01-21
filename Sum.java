acfd07886604fba38081f2103d7e2dfa023b83d6
harry <cm@mptc>
2017-01-21 14:50:37 +0530

    Sum added

---
+++ Sum.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_training;
+
+/**
+ *
+ * @author HP
+ */
+public class Sum {
+    
+    public static void main(String[] args){
+        int a = 5;
+        int b = 6;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + "=" + sum) ;
+    }
+    
+}
